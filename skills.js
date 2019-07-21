1675e3e0e2c0f1516f97f7b7195b2808ff73053b
Brinley Macnamara <bmacnamara@mitre.org>
2019-07-21 18:08:36 -0400

    added DOM loaded event listener to skills.js

--- skills.js
+++ skills.js
@@ -41,29 +41,36 @@ data = [
     }
 ];
 
-const margin = 60;
-const width = 1000 - 2 * margin;
-const height = 600 - 2 * margin;
+document.addEventListener("DOMContentLoaded", function(e) {
 
-const svg = d3.select("svg");
-const svgContainer = d3.select("#container");
+    console.log("DOM loaded!");
 
-const chart = svg.append("g")
-    .attr("transform", `translate(${margin}, ${margin})`);
+    const margin = 60;
+    const width = 1000 - 2 * margin;
+    const height = 600 - 2 * margin;
+    
+    const svg = d3.select("svg");
+    const svgContainer = d3.select("#container");
+    
+    const chart = svg.append("g")
+        .attr("transform", `translate(${margin}, ${margin})`);
+    
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, 100]);
+    
+    chart.append("g")
+        .call(d3.axisLeft(yScale));
+    
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(data.map((s) => s.language))
+        .padding(0.2)
+    
+    chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
 
-const yScale = d3.scaleLinear()
-    .range([height, 0])
-    .domain([0, 100]);
+});
 
-chart.append("g")
-    .call(d3.axisLeft(yScale));
-
-const xScale = d3.scaleBand()
-    .range([0, width])
-    .domain(data.map((s) => s.language))
-    .padding(0.2)
-
-chart.append('g')
-    .attr('transform', `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale));
 
