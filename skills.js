b653535a3ddb8680d34c1a34caa687cf181355d3
Brinley Macnamara <bmacnamara@mitre.org>
2019-07-21 17:59:59 -0400

    added styles to skills page

--- skills.js
+++ skills.js
@@ -46,6 +46,7 @@ const width = 1000 - 2 * margin;
 const height = 600 - 2 * margin;
 
 const svg = d3.select("svg");
+const svgContainer = d3.select("#container");
 
 const chart = svg.append("g")
     .attr("transform", `translate(${margin}, ${margin})`);
